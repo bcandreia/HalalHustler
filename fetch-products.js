a609f670d9d2b9f16e701fdff11ba7ed54b1df46
bcandreia <bcandreia@gmail.com>
2025-08-06 12:43:49 -0700

    Update fetch-products.js

--- fetch-products.js
+++ fetch-products.js
@@ -33,14 +33,14 @@ https
             currency: "USD",
           }).format(price);
 
-          // Use handle if it exists; otherwise fallback to product.id
-          const handle = product.handle ? product.handle : product.id;
+          // Fix: use product.handle if it exists, otherwise use product.id
+          const handleOrId = product.handle ? product.handle : product.id;
 
           return {
             title: product.title || "No title",
             image: image || "",
             price: formattedPrice,
-            link: `https://halal-hustler.printify.me/products/${handle}`,
+            link: `https://halal-hustler.printify.me/products/${handleOrId}`,
           };
         });
 
